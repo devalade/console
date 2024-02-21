e73aaf95c1164c5763651af55de3bb809a58d140
Alexis Bouchez <bouchezalexis1@gmail.com>
2024-02-21 09:48:14 +0100

    Fix presence

--- resources/components/avatar.tsx
+++ resources/components/avatar.tsx
@@ -12,21 +12,23 @@ const Avatar: React.FunctionComponent<AvatarProps> = ({ user }) => {
   const presence = usePresence()
   const isOnline = presence.includes(user.id)
   return (
-    <span className="relative inline-block">
-      <div className="relative">
-        <div className="flex h-8 w-8 items-center justify-center rounded-full bg-zinc-800 border-zinc-700 border text-zinc-200">
-          <span className="font-medium">{getInitials(user.fullName)}</span>
+    <div>
+      <span className="relative inline-block">
+        <div className="relative">
+          <div className="flex h-8 w-8 items-center justify-center rounded-full bg-zinc-800 border-zinc-700 border text-zinc-200">
+            <span className="font-medium">{getInitials(user.fullName)}</span>
+          </div>
         </div>
-      </div>
-      <span className="absolute bottom-1 right-1 block translate-x-1/2 translate-y-1/2 transform rounded-full border-2 border-white">
-        <span
-          className={clsx(
-            'block h-[7px] w-[7px] rounded-full',
-            isOnline ? 'bg-emerald-500' : 'bg-red-500'
-          )}
-        />
+        <span className="absolute bottom-1 right-1 block translate-x-1/2 translate-y-1/2 transform rounded-full border-2 border-white">
+          <span
+            className={clsx(
+              'block h-[7px] w-[7px] rounded-full',
+              isOnline ? 'bg-emerald-500' : 'bg-red-500'
+            )}
+          />
+        </span>
       </span>
-    </span>
+    </div>
   )
 }
 
--- resources/layouts/project_layout.tsx
+++ resources/layouts/project_layout.tsx
@@ -7,6 +7,7 @@ import { IconArrowBackUp } from '@tabler/icons-react'
 import useProjectLayoutNavigationItems from '@/concerns/projects/hooks/use_project_layout_navigation_items'
 import useCurrentProject from '@/concerns/projects/hooks/use_current_project'
 import useParams from '@/hooks/use_params'
+import { PresenceProvider } from '@/presence_context'
 
 interface ProjectLayoutProps extends React.PropsWithChildren {
   className?: string
@@ -19,23 +20,25 @@ const ProjectLayout: React.FunctionComponent<ProjectLayoutProps> = ({ children, 
   const navigationItems = useProjectLayoutNavigationItems(currentProject)
 
   return (
-    <SharedLayout
-      children={children}
-      className={className}
-      sidebarHeaderChildren={
-        <ProjectSelector projects={projects} currentProjectSlug={params.projectSlug} />
-      }
-      sidebarFooterChildren={
-        <Link
-          className="text-sm flex items-center text-zinc-900 hover:opacity-50 transition space-x-2"
-          href="/projects"
-        >
-          <IconArrowBackUp size={20} stroke={1} />
-          <span>Back to projects</span>
-        </Link>
-      }
-      navigationItems={navigationItems}
-    />
+    <PresenceProvider>
+      <SharedLayout
+        children={children}
+        className={className}
+        sidebarHeaderChildren={
+          <ProjectSelector projects={projects} currentProjectSlug={params.projectSlug} />
+        }
+        sidebarFooterChildren={
+          <Link
+            className="text-sm flex items-center text-zinc-900 hover:opacity-50 transition space-x-2"
+            href="/projects"
+          >
+            <IconArrowBackUp size={20} stroke={1} />
+            <span>Back to projects</span>
+          </Link>
+        }
+        navigationItems={navigationItems}
+      />
+    </PresenceProvider>
   )
 }
 
